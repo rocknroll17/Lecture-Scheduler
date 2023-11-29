02ab86d869854fd2a173f04b1dd5196353b5eeea
Jaehwan Hyun <jec0101@naver.com>
2023-11-29 14:06:12 +0900

    initialized timeblock

---
+++ TimeTablePackage/Timeblock.py
@@ -0,0 +1,9 @@
+class Timeblock():
+    def __init__(self):
+        pass
+    def __init__(self, day, period, courseTime):
+        pass
+    def __str__(self):
+        pass
+    def __repr__(self):
+        pass
