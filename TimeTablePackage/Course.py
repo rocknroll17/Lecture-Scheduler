c94646d3a4b59bab3639ff20964847cb54e4602c
Jaehwan Hyun <jec0101@naver.com>
2023-11-29 14:00:17 +0900

    객체변수로 바꿈

--- TimeTablePackage/Course.py
+++ TimeTablePackage/Course.py
@@ -1,15 +1,14 @@
 class Course():
-    college = "" # 대학
-    department = "" # 개설학과
-    isMajor = "" # 이수구분 - 전공인지 -> 필요한가??
-    grade = 0 # 학년 -> 필요한가??
-    courseId = 0 # 과목번호
-    title = "" # 과목명
-    credit = 0 # 학점
-    instructor = "" # 강의자
-    #vector<Timeblock> courseTime # 강의시간. Timeblock 배열에 저장.
-    courseType = "" # 강의유형 -> 필요한가??
-    annotations = "" # 비고 -> 필요한가??
-
     def __init__(self, one_course):
         # one_course 문자열 파싱해서 클래스변수 저장
+        self.college = "" # 대학
+        self.department = "" # 개설학과
+        self.isMajor = "" # 이수구분 - 전공인지 -> 필요한가??
+        self.grade = 0 # 학년 -> 필요한가??
+        self.courseId = 0 # 과목번호
+        self.title = "" # 과목명
+        self.credit = 0 # 학점
+        self.instructor = "" # 강의자
+        #self.vector<Timeblock> courseTime # 강의시간. Timeblock 배열에 저장.
+        self.courseType = "" # 강의유형 -> 필요한가??
+        self.annotations = "" # 비고 -> 필요한가??
