8425ddf461e922d08ae862aa40158f8794b02a92
Jaehwan Hyun <jec0101@naver.com>
2023-11-29 14:26:53 +0900

    Timeblock 대충 만듬 (TODO: public private 설정)

--- TimeTablePackage/Timeblock.py
+++ TimeTablePackage/Timeblock.py
@@ -1,9 +1,16 @@
 class Timeblock():
     def __init__(self):
-        pass
+        self.day = ""  # 요일 "Monday" ~ "Sunday"
+        self.period = 0  # N교시
+        self.courseTime = "0:00" # H:MM -> H시간 MM분 강의
+        
     def __init__(self, day, period, courseTime):
-        pass
+        self.day = day
+        self.period = period
+        self.courseTime = courseTime
+        
     def __str__(self):
         pass
+    
     def __repr__(self):
         pass
