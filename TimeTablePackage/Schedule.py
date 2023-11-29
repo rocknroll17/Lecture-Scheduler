f3c14a7679f4844fb9080a58f379a421173cff89
Jaehwan Hyun <jec0101@naver.com>
2023-11-30 00:42:48 +0900

    일단 만듬

--- TimeTablePackage/Schedule.py
+++ TimeTablePackage/Schedule.py
@@ -1,1 +1,13 @@
 # 시간표 객체
+# 완성된 하나의 시간표 객체
+# 사용자가 듣고 싶은 강의 담겨있음
+# 이게 GUI에 시간표 탭에 뜨게 할 예정
+
+class Schedule:
+    def __init__(self):
+        self.__courses = [] 
+        # 담은 강의 - 시간 순으로 정렬해서 유지 -> 이진트리??
+        # private임
+    def add_course(self, course):
+        self.__courses.append(course)
+        self.__courses.sort(key=lambda x : x.time.period)
