28327b5df287a6cdb174e133e15d1997333d1893
rocknroll <gon1397@naver.com>
2023-12-01 00:50:14 +0900

    Basket

---
+++ Basket.py
@@ -0,0 +1,27 @@
+class Candidate:
+    #꼭 들으면 좋음
+    def __init__(self):
+        self.data = []
+    
+    def add(self, group):
+        self.data.append(group)
+
+    def delete(self, index):
+        del self.data[index]
+
+    def remove(self, group):
+        self.data.remove(group)
+
+class basket:
+    #장바구니
+    def __init__(self):
+        self.data = []
+
+    def add(self, lecture):
+        self.data.append(lecture)
+
+    def delete(self, index):
+        del self.data[index]
+
+    def remove(self, lecture):
+        self.data.remove(lecture)
