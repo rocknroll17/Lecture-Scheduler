af20d36a32657fdb771fb67ac12ef0b1859eab41
Jaehwan Hyun <jec0101@naver.com>
2023-12-02 04:30:36 +0900

    Basket에 강의존재확인 기능 추가 (exists(course))

--- Basket.py
+++ Basket.py
@@ -1,8 +1,11 @@
+from collections import defaultdict
+
 class Candidate:
     #꼭 들으면 좋음
     def __init__(self):
         self.data = []
-    
+        self.__ids = defaultdict(bool) # 강의 존재하는지 확인용
+
     def add(self, group):
         self.data.append(group)
 
@@ -16,12 +19,18 @@ class Basket:
     #장바구니
     def __init__(self):
         self.data = []
+        self.__ids = defaultdict(bool) # 강의 존재하는지 확인용
 
     def add(self, lecture):
         self.data.append(lecture)
+        self.__ids[lecture.course_id] = True
 
     def delete(self, index):
         del self.data[index]
 
     def remove(self, lecture):
         self.data.remove(lecture)
+        self.__ids.pop(lecture.course_id)
+    
+    def exists(self, lecture):
+        return self.__ids[lecture.course_id]
