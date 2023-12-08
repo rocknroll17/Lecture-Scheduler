bdf8935caffa17e45bf0b00fdb4a6998e0e7dfe7
rocknroll <gon1397@naver.com>
2023-12-08 20:31:47 +0900

    그룹 삭제, 다른 버튼 간에 기존 활성화 버튼 안 보이게함

--- main.py
+++ main.py
@@ -483,6 +483,14 @@ class Magic(QMainWindow, form_class2, SaveOnClose):
             # 그룹박스 이미 띄워져있으면 창 지움 2
             self.prefer_button_group.setVisible(False)
             return
+        if self.delete_must_button_group.isVisible():
+            # 그룹박스 이미 띄워져있으면 창 지움 2
+            self.delete_must_button_group.setVisible(False)
+            return
+        if self.delete_prefer_button_group.isVisible():
+            # 그룹박스 이미 띄워져있으면 창 지움 2
+            self.delete_prefer_button_group.setVisible(False)
+            return
         # 원래 들어가있었던 버튼 그룹 삭제  (ex) ['추가하지 않음','그룹 1','그룹 추가'])
         self.layout().removeWidget(self.must_button_group)
         while self.must_group_layout.count():
@@ -580,6 +588,14 @@ class Magic(QMainWindow, form_class2, SaveOnClose):
         if self.must_button_group.isVisible():
             # 이미 띄워져 있으면 창 지움 2
             self.must_button_group.setVisible(False)
+            return
+        if self.delete_must_button_group.isVisible():
+            # 그룹박스 이미 띄워져있으면 창 지움 2
+            self.delete_must_button_group.setVisible(False)
+            return
+        if self.delete_prefer_button_group.isVisible():
+            # 그룹박스 이미 띄워져있으면 창 지움 2
+            self.delete_prefer_button_group.setVisible(False)
             return
         self.layout().removeWidget(self.prefer_button_group)
         while self.prefer_group_layout.count():
@@ -722,6 +738,14 @@ class Magic(QMainWindow, form_class2, SaveOnClose):
             # 들으면좋음 삭제창 띄워져있으면 지움
             self.delete_prefer_button_group.setVisible(False)
             return
+        if self.prefer_button_group.isVisible():
+            # 그룹박스 이미 띄워져있으면 창 지움
+            self.prefer_button_group.setVisible(False)
+            return
+        if self.must_button_group.isVisible():
+            # 이미 띄워져 있으면 창 지움 2
+            self.must_button_group.setVisible(False)
+            return
         if Must_layout:
             xButton = QPushButton('삭제 안 함')
             xButton.clicked.connect(self.must_removeGroup2)
@@ -854,6 +878,14 @@ class Magic(QMainWindow, form_class2, SaveOnClose):
         if self.delete_must_button_group.isVisible():
             # 꼭 삭제창 띄워져있어도 창 지움
             self.delete_must_button_group.setVisible(False)
+            return
+        if self.prefer_button_group.isVisible():
+            # 그룹박스 이미 띄워져있으면 창 지움
+            self.prefer_button_group.setVisible(False)
+            return
+        if self.must_button_group.isVisible():
+            # 이미 띄워져 있으면 창 지움 2
+            self.must_button_group.setVisible(False)
             return
         if Prefer_layout:
             xButton = QPushButton('삭제 안 함')
